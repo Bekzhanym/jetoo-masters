fef68cabd7f7f23ce2eaab65e5ff3503e707e4ea
Bekzhanym <maukenovbekzan@gmail.com>
2025-09-08 10:12:12 +0500

    main page

--- src/components/WelcomeScreen.tsx
+++ src/components/WelcomeScreen.tsx
@@ -9,14 +9,7 @@ const WelcomeScreen: React.FC<WelcomeScreenProps> = ({ onStart }) => {
   return (
     <div className="welcome-screen">
       <div className="welcome-container">
-        <div className="welcome-content">
-          <h1 className="welcome-title">JETOO ENGLISH</h1>
-          <p className="welcome-subtitle">Тест на знание английского языка</p>
-          <p className="welcome-description">
-            Проверьте свой уровень английского языка с помощью нашего интерактивного теста
-          </p>
-        </div>
-        
+
         {/* Анимированная кнопка */}
         <div className="animated-button-container">
           <button 
